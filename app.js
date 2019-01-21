35351d51272b17f185f46fbd58fc96fbd9a56db8
Michael Desroches <desrochesmichael18@gmail.com>
2019-01-20 21:51:16 -0500

    Fast Track

---
+++ app.js
@@ -0,0 +1,63 @@
+
+const list = document.querySelector('#book-list ul');
+
+list.addEventListener('click', function(e){
+    if(e.target.className == 'delete'){
+        const li = e.target.parentElement;
+        list.removeChild(li)
+    }
+})
+
+const addForm = document.forms['add-book'];
+
+addForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    const value = addForm.querySelector('input[type="text"]').value;
+    
+    const li = document.createElement('li');
+    const bookName = document.createElement('span');
+    const deleteBtn = document.createElement('span');
+
+    deleteBtn.textContent = 'delete';
+    bookName.textContent = value;
+
+    bookName.classList.add('name');
+    deleteBtn.classList.add('delete');
+
+
+    li.appendChild(bookName);
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+})
+
+const hideBox = document.querySelector('#hide');
+hideBox.addEventListener('change', function(e){
+    if (hideBox.checked){
+        list.style.display = "none";
+    } else {
+        list.style.display = "initial";
+    }
+})
+
+const searchBar = document.forms['search-books'].querySelector('input');
+searchBar.addEventListener('keyup', function(e){
+    const term = e.target.value.toLowerCase();
+    const books = list.getElementsByTagName('li');
+    Array.from(books).forEach(function(book){
+        const title = book.firstElementChild.textContent;
+        if(title.toLowerCase().indexOf(term)!= -1){
+            book.style.display = 'block';
+        } else {
+            book.style.display = 'none';
+        }
+    });
+})
+
+
+
+
+
+
+
+
+
